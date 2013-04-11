5fdb200a7ed464ffd29c6637178ea143ac70cbd3
Kinjal Patel <kpatel@tweddle.com>
2013-04-11 12:22:54 -0700

    Java MainJava.class changes

--- src/MainJava.java
+++ src/MainJava.java
@@ -3,7 +3,9 @@ public class MainJava {
 	
  public static void main(String[] args) {
 	 System.out.println("MainJava.main()");
-	
+	 
+	String hex = StringUtils.convertToHex("qeq".getBytes());
+	System.out.println("MainJava.main() "+hex);
 }
 
 }
