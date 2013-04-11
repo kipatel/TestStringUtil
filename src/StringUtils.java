44833e1025ddc3225efdfa2378438926c8ba811a
Kinjal Patel <kpatel@tweddle.com>
2013-04-11 12:19:42 -0700

    Initial commit

---
+++ src/StringUtils.java
@@ -0,0 +1,27 @@
+
+public class StringUtils {
+	
+	public static String convertToHex(byte[] data) {
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < data.length; i++) {
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do {
+				if ((0 <= halfbyte) && (halfbyte <= 9))
+					buf.append((char) ('0' + halfbyte));
+				else
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} while (two_halfs++ < 1);
+		}
+		return buf.toString();
+	}
+	public static byte[] hex2Byte(String str) {
+		byte[] bytes = new byte[str.length() / 2];
+		for (int i = 0; i < bytes.length; i++) {
+			bytes[i] = (byte) Integer.parseInt(str.substring(2 * i, 2 * i + 2), 16);
+		}
+		return bytes;
+	}
+
+}
